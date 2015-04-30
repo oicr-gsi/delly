ac2e1cd68f7a78be716d72f20e003c13c26fcc3b
Peter Ruzanov <pruzanov@gmail.com>
2015-04-30 16:31:31 -0400

    Incremental modiffications to BICseq configuring scripts

--- workflow/dependencies/configureBICseq.pl
+++ workflow/dependencies/configureBICseq.pl
@@ -1,53 +1,67 @@
 #!/usr/bin/perl -w
 
 use strict;
-use Getopt:Long;
+use Getopt::Long;
+use File::Basename;
 use constant DEBUG=>1;
 
-my $USAGE = "configureBICseq.pl --input-t [tumor input] --input-n [normal input] --outdir [root data dir] --bicseq-interval [I] --bicseq-spread [S] --samtools [path to modified samtools]\n";
-my($input_n,$input_t,$output,$samtools);
-my $results = GetOption("--input-n=s"  => \$input_n,
-                        "--input-t=s"  => \$input_t,
-                        "--outdir=s"   => \$datadir,
-                        "--bicseq-interval=i" => \$bicseq_interval,
-                        "--bicseq-spread=i" => \$bicseq_spread,
-                        "--samtools=s" => \$samtools);
-
+my $USAGE = "configureBICseq.pl --input-t [tumor input] --input-n [normal input] --outdir [root data dir] --config-file [name of config file] --samtools [path to modified samtools]\n";
+my($input_n,$input_t,$datadir,$config,$samtools);
+my $results = GetOptions ("input-n=s"  => \$input_n,
+                          "input-t=s"  => \$input_t,
+                          "outdir=s"   => \$datadir,
+                          "config-file=s"=>\$config,
+                          "samtools=s" => \$samtools);
+if (!$input_t || !$input_n || !$datadir || !$config || !$samtools){die $USAGE;}
 my @inputs = ($input_n, $input_t);
-my %info = ();
+my %info   = ();
+my @chroms = ();
 map{&process_bam} @inputs;
 
+#=====================================
+# Configuring BICseq run here
+#=====================================
+my @chromlines = grep {/^\@SQ/} `$samtools view -H $input_n`;
+# TODO handle ref-free experiments
+print STDERR "Got ".scalar(@chromlines)." chromosomes from bam file\n" if DEBUG;
 
+my $tumorSeq  = basename($input_t,(".bam")).".seq";
+my $normalSeq = basename($input_n,(".bam")).".seq";
 
+print STDERR "Got $tumorSeq and $normalSeq for analysis\n" if DEBUG;
 
+open(CONF,">$datadir/$config") or die "Could not create config file for BICseq";
+print CONF join("\t",("chrom","tumor","normal"))."\n";
+foreach my $line (@chromlines) {
+  if ($line!~/SN\:(\S+)/){next;}
+    my $c = $1;
+    print STDERR "Found chromosome $c\n" if DEBUG;
+    print CONF join("\t",($c,$tumorSeq,$normalSeq))."\n";
+}
 
+close CONF;
 
-
-
-#
+#======================================
 # Subroutine for parsing .bam into .seq
-#
-
+#======================================
 sub process_bam {
  my $bam = shift @_;
- my $seqfile = $bam;
- $seqfile=~s/.bam$/.seq/;
 
  if (!-e $bam && !-s $bam) {
   return;
  }
  
- $pg = grep {/^\@PG/} `$samtools view -H $bam`;
+ my $pg = grep {/^\@PG/} `$samtools view -H $bam`;
  if ($pg && $pg=~/ID:(\S+)/) {
    my $aligner = $1;
    if ($aligner=~/BWA/) {
-     `$samtools view -U BWA,$datadir,`;
+     `$samtools view -U BWA,$datadir,N,N $bam`;
    } elsif ($aligner=~/Bowtie/) {
-     ``;
-
+     `$samtools view -U Bowtie,$datadir,N,N $bam`;
    } else {
-
-
+     print STDERR "BICseq supports Bowtie and BWA aligners only, will terminate\n";
+     exit;
    }
  }
 }
+
