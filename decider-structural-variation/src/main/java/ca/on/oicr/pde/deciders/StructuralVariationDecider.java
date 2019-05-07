7dd6de7566d47f0b18f28eca23560fb5c7824768
Peter Ruzanov <pruzanov@gmail.com>
2019-05-07 16:05:03 -0400

    Fixed template-type parameter

--- decider-structural-variation/src/main/java/ca/on/oicr/pde/deciders/StructuralVariationDecider.java
+++ decider-structural-variation/src/main/java/ca/on/oicr/pde/deciders/StructuralVariationDecider.java
@@ -11,7 +11,8 @@ import java.util.*;
 import net.sourceforge.seqware.common.hibernate.FindAllTheFiles.Header;
 import net.sourceforge.seqware.common.module.FileMetadata;
 import net.sourceforge.seqware.common.module.ReturnValue;
-import net.sourceforge.seqware.common.util.Log;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import net.sourceforge.seqware.common.util.maptools.MapTools;
 
 /**
@@ -19,7 +20,7 @@ import net.sourceforge.seqware.common.util.maptools.MapTools;
  * @author pruzanov@oicr.on.ca
  */
 public class StructuralVariationDecider extends OicrDecider {
-
+    private final Logger logger = LogManager.getLogger(OicrDecider.class);
     private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
     private Map<String, BeSmall> fileSwaToSmall;
     private String templateTypeFilter = "";
@@ -40,6 +41,7 @@ public class StructuralVariationDecider extends OicrDecider {
     public StructuralVariationDecider() {
         super();
         fileSwaToSmall  = new HashMap<String, BeSmall>();
+        
         parser.acceptsAll(Arrays.asList("ini-file"), "Optional: the location of the INI file.").withRequiredArg();
         
         parser.accepts("picard-memory","Optional. Set the memory allocated to java heap "
@@ -49,8 +51,7 @@ public class StructuralVariationDecider extends OicrDecider {
         parser.accepts("queue", "Optional: Override the default queue setting (production) setting it to something else").withRequiredArg();
         parser.accepts("ref-fasta", "Optional: the path to reference fasta used by Delly "
 	        + " Default: /.mounts/labs/PDE/data/reference/hg19_random/fasta/UCSC/hg19_random.fa").withRequiredArg();
-        parser.accepts("exclude-list", "Optional: the path to file with targets not used by Delly "
-	        + " Default: /.mounts/labs/PDE/data/reference/hg19/delly/human.hg19.excl.tsv").withRequiredArg();
+        parser.accepts("template-type", "Optional: may be used as a filter to restrict the analysis to a certain template type").withRequiredArg();
         parser.accepts("mode", "Required: Either somatic or unmatched").withRequiredArg();
         parser.accepts("exclude-list", "Optional: the path to file with targets not used by Delly "
 	        + " Default: /.mounts/labs/PDE/data/reference/hg19/delly/human.hg19.excl.tsv").withRequiredArg();
@@ -59,7 +60,7 @@ public class StructuralVariationDecider extends OicrDecider {
 
     @Override
     public ReturnValue init() {
-        Log.stdout("INIT");
+        logger.info("INIT");
 	this.setMetaType(Arrays.asList(BAM_METATYPE)); // It seems that this does not have effect 
                         
         //Handle .ini file - we accept only memeory size allocated to different steps
@@ -73,14 +74,14 @@ public class StructuralVariationDecider extends OicrDecider {
 		this.delly_memory = iniFileMap.get("delly_memory");
 		this.picard_memory = iniFileMap.get("picard_memory");
             } else {
-                Log.stderr("The given INI file does not exist: " + file.getAbsolutePath());
+                 logger.error("The given INI file does not exist: " + file.getAbsolutePath());
                 System.exit(1);
             }
 	}
 
 	//Group by sample if no other grouping selected
         if (this.options.has("group-by")) {
-            Log.stderr("group-by parameter passed, but this decider does not allow overriding the default grouping, which is group-by FILE_SWA");
+            logger.info("group-by parameter passed, but this decider does not allow overriding the default grouping, which is group-by FILE_SWA");
         }
         
        	if (this.options.has("root-sample-name")) {
@@ -93,42 +94,42 @@ public class StructuralVariationDecider extends OicrDecider {
         
         if (this.options.has("template-type")) {
             this.templateTypeFilter = options.valueOf("template-type").toString();
-            Log.stdout("Setting template type is not necessary, however if set the decider will run the workflow only on this type of data");
+            logger.info("Setting template type is not necessary, however if set the decider will run the workflow only on this type of data");
 	}
         
         if (this.options.has("picard-memory")) {
             this.picard_memory = options.valueOf("picard-memory").toString();
-            Log.stdout("Setting heap size for Picard step to " + this.picard_memory + " Megabytes as requested");
+            logger.info("Setting heap size for Picard step to " + this.picard_memory + " Megabytes as requested");
 	}
         
         if (this.options.has("delly-memory")) {
             this.picard_memory = options.valueOf("delly-memory").toString();
-            Log.stdout("Setting memory amount for Delly step to " + this.delly_memory + " Megabytes as requested");
+            logger.info("Setting memory amount for Delly step to " + this.delly_memory + " Megabytes as requested");
 	}
 
         if (this.options.has("ref-fasta")) {
             this.refFasta = options.valueOf("ref-fasta").toString();
-            Log.stdout("Changing ref-fasta file to " + this.refFasta);
+            logger.info("Changing ref-fasta file to " + this.refFasta);
         }
         
         if (this.options.has("exclude-list")) {
             this.excludeList = options.valueOf("exclude-list").toString();
-            Log.stdout("Changing exclude-list file to " + this.excludeList);
+            logger.info("Changing exclude-list file to " + this.excludeList);
         }
         
         if (this.options.has("mapping-quality")) {
             this.mappingQuality = options.valueOf("mapping-quality").toString();
-            Log.stdout("Changing mapping-quality " + this.mappingQuality);
+            logger.info("Changing mapping-quality " + this.mappingQuality);
         }
 
         if (this.options.has("mode")) {
              this.callMode = options.valueOf("mode").toString();
               if (!this.callMode.equals(SOMATIC) && !this.callMode.equals(UNMATCHED)) {
-                 Log.stderr("Mode needs to be specified as either unmatched or somatic");
+                 logger.error("Mode needs to be specified as either unmatched or somatic");
                  return new ReturnValue(ReturnValue.INVALIDPARAMETERS);                 
               }
         } else {
-            Log.stderr("Mode is a required parameter and needs to be specified");
+            logger.error("Mode is a required parameter and needs to be specified");
             return new ReturnValue(ReturnValue.INVALIDPARAMETERS);
         }
 
@@ -160,7 +161,7 @@ public class StructuralVariationDecider extends OicrDecider {
         
         for (String p : filePaths) {
              if (null != this.duplicates && this.duplicates.contains(p)) {
-                Log.stderr("File [" + p + "] has a name that cannot be disambiguated in current set, will skip it");
+                logger.error("File [" + p + "] has a name that cannot be disambiguated in current set, will skip it");
                 continue;
             }
              
@@ -185,12 +186,12 @@ public class StructuralVariationDecider extends OicrDecider {
         }
 
         if (tumorFiles > 1 && (tumorFiles != groupIds)) {
-            Log.error("We have multiple tumor files but not all of them have group id value, WON'T RUN");
+            logger.error("We have multiple tumor files but not all of them have group id value, WON'T RUN");
             return new ReturnValue(ReturnValue.INVALIDPARAMETERS);
         }
         
         if (normalFiles > 1) {
-            Log.error("We have multiple normal files but we can only run the analysis with one normal, WON'T RUN");
+            logger.error("We have multiple normal files but we can only run the analysis with one normal, WON'T RUN");
             return new ReturnValue(ReturnValue.INVALIDPARAMETERS);
         }
 
@@ -199,13 +200,13 @@ public class StructuralVariationDecider extends OicrDecider {
         }
 
         String absent = haveNorm ? "Tumor" : "Normal";
-        Log.error("Data for " + absent + " tissue are not available, WON'T RUN");
+        logger.error("Data for " + absent + " tissue are not available, WON'T RUN");
         return new ReturnValue(ReturnValue.INVALIDPARAMETERS);
     }
 
     @Override
     protected boolean checkFileDetails(ReturnValue returnValue, FileMetadata fm) {
-        Log.stdout("CHECK FILE DETAILS:" + fm.getFilePath());
+        logger.info("CHECK FILE DETAILS:" + fm.getFilePath());
 	this.sampleName      = returnValue.getAttribute(Header.SAMPLE_NAME.getTitle());
         String tissueType    = returnValue.getAttribute(Header.SAMPLE_TAG_PREFIX.getTitle() + "geo_tissue_type");
         String tissueOrigin  = returnValue.getAttribute(Header.SAMPLE_TAG_PREFIX.getTitle() + "geo_tissue_origin");
@@ -217,7 +218,7 @@ public class StructuralVariationDecider extends OicrDecider {
         String currentTtype    = returnValue.getAttribute(Header.SAMPLE_TAG_PREFIX.getTitle() + "geo_library_source_template_type");
         // Filter the data of a different template type if filter is specified
         if (!this.templateTypeFilter.isEmpty() && !this.templateTypeFilter.equalsIgnoreCase(currentTtype)) {
-            Log.stderr("Template type " + currentTtype + " does not pass filter which is set to " + this.templateTypeFilter);
+            logger.error("Template type " + currentTtype + " does not pass filter which is set to " + this.templateTypeFilter);
             return false;
         }
         return super.checkFileDetails(returnValue, fm);
@@ -240,7 +241,7 @@ public class StructuralVariationDecider extends OicrDecider {
                  if (currentRV.getFiles().get(f).getMetaType().equals(BAM_METATYPE))
                      metatypeOK = true;
                } catch (Exception e) {
-                 Log.stderr("Error checking a file");
+                 logger.error("Error checking a file");
                }
             }
             if (!metatypeOK)
@@ -256,7 +257,7 @@ public class StructuralVariationDecider extends OicrDecider {
             
             //if there is no entry yet, add it
             if (iusDeetsToRV.get(fileDeets) == null) {
-                Log.debug("Adding file " + fileDeets + " -> \n\t" + currentSmall.getPath());
+                logger.debug("Adding file " + fileDeets + " -> \n\t" + currentSmall.getPath());
                 iusDeetsToRV.put(fileDeets, currentRV);
             } 
             //if there is an entry, compare the current value to the 'old' one in
@@ -268,15 +269,15 @@ public class StructuralVariationDecider extends OicrDecider {
                 BeSmall oldSmall = fileSwaToSmall.get(oldRV.getAttribute(Header.FILE_SWA.getTitle()));
                 Date oldDate = oldSmall.getDate();
                 if (currentDate.after(oldDate)) {
-                    Log.debug("Adding file " + fileDeets + " -> \n\t" + currentSmall.getDate()
+                    logger.debug("Adding file " + fileDeets + " -> \n\t" + currentSmall.getDate()
                             + "\n\t instead of file "
                             + "\n\t" + oldSmall.getDate());
                     iusDeetsToRV.put(fileDeets, currentRV);
                 } else {
-                    Log.debug("Disregarding file " + fileDeets + " -> \n\t" + currentSmall.getDate()
+                    logger.debug("Disregarding file " + fileDeets + " -> \n\t" + currentSmall.getDate()
                             + "\n\tas older than duplicate sequencer run/lane/barcode in favour of "
                             + "\n\t" + oldSmall.getDate());
-                    Log.debug(currentDate + " is before " + oldDate);
+                    logger.debug(currentDate + " is before " + oldDate);
                 }
             }
         }
@@ -418,7 +419,7 @@ public class StructuralVariationDecider extends OicrDecider {
             try {
                 date = format.parse(rv.getAttribute(Header.PROCESSING_DATE.getTitle()));
             } catch (ParseException ex) {
-                Log.error("Bad date!", ex);
+                logger.error("Bad date!", ex);
                 ex.printStackTrace();
             }
             FileAttributes fa = new FileAttributes(rv, rv.getFiles().get(0));
