bd2e7d987e509a5ef7f469012a5c126a981c9c38
pruzanov <pruzanov@oicr.on.ca>
2021-11-10 17:20:50 -0500

    Making RT tests more robust

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,5 +6,5 @@ cd $1
 # - Check md5sums for all types of files, sort
 
 echo ".vcf files:"
-for v in *.vcf.gz;do zcat $v | grep -v Date | grep -v ^##fileDate | grep ^#;done
+for v in *.vcf.gz;do zcat $v | grep -v Date | grep -v ^##fileDate | md5sum;done | sort
 
